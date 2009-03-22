22ab0f3ac53b75292bfed8f7981fc55aea277259
kyllerss <kyllerss@hotmail.com>
2009-03-21 20:45:04 -0400

    Updated documentation. Cleaned up KeyGenerator. Added stub for Encryptor.

---
+++ lib/encryptor.rb
@@ -0,0 +1,14 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class Encryptor
+
+  def initialize
+    
+  end
+
+  def encrypt(plain_text)
+
+    
+  end
+end
--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -6,13 +6,21 @@ class KeyGenerator
   def initialize(key)
 
     @round_keys = Array.new
-
     round_constant_manager = RoundConstants.new
     input_state = key
+
+    @round_keys << input_state
     1.upto(10) do |i|
       round = Round.new(input_state, round_constant_manager.get_round_constant(i))
       @round_keys << round
       input_state = round.state # will be used as input state for next Round
     end
+  end
+
+  def get_round_key(round_key_num)
+
+    raise "Unknown round key requested. Must be 0..10." unless 0..10 === round_key_num
+
+    @round_key[round_key_num]
   end
 end
